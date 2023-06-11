a845defe4e97131503f7eeae3ac226835395a3ca
Imagine Studio <cto@imaginestudio.io>
2023-06-11 06:45:47 -0700

    Create main.go

---
+++ Functional-Programming/Chapter4/Examples/HotdogShop/PureHotdogShop/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ImagineDevOps/Chapter4/Examples/HotdogShop/PureHotdogShop/pkg"
+)
+
+func main() {
+	myCard := pkg.NewCreditCard(1000)
+	hotdog, creditFunc := pkg.OrderHotdog(myCard, pkg.Charge)
+	fmt.Printf("%+v\n", hotdog)
+	newCard, err := creditFunc()
+	if err != nil {
+		panic("User has no credit")
+	}
+	myCard = newCard
+	fmt.Printf("%+v\n", myCard)
+}

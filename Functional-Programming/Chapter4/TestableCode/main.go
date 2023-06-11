daef5c87e3892746ffb1c1c3e21e9f2e890401df
Imagine Studio <cto@imaginestudio.io>
2023-06-11 06:45:58 -0700

    Create main.go

---
+++ Functional-Programming/Chapter4/TestableCode/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"math/rand"
+
+	"github.com/ImagineDevOps/Chapter4/TestableCode/player"
+)
+
+func main() {
+	random := rand.Intn(2)
+	player.PlayerSelectPure(random)
+	// start the game
+}
